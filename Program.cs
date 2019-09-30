b1a8fe1e99eed9fda959fc54e1516c87d6e5834d
sameer.saran <sameer.saran@visioncritical.com>
2019-09-30 11:09:38 -0700

    Added MD5 hash

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace ConsoleApp2
 {
@@ -11,6 +12,8 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             string password = "Testpassword";
+            MD5CryptoServiceProvider md5hash = new MD5CryptoServiceProvider();
+            byte[] hashArray = md5hash.ComputeHash(Encoding.ASCII.GetBytes(password));
             Console.WriteLine("password is " + password);
         }
     }
